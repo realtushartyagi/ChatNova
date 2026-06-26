eed7d06b3bb35f862bafa942e9eac1c767c5f75e
realtushartyagi <tushartyagi7575@gmail.com>
2026-06-26 07:30:41 +0530

    fix: increase express json body limit to 50mb to allow large base64 image uploads

--- server/server.js
+++ server/server.js
@@ -26,7 +26,8 @@ app.use(async (req, res, next) => {
 // Stripe Webhooks
 app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 // Routes
 app.get('/', (req, res)=> res.send('Server is Live!'))
